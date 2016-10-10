9294bbfb12b3a8f59326f53a1ecf9d356804d504
yu3mars <yu3mars@users.noreply.github.com>
2016-10-11 01:29:48 +0900

    add geometory3d(true)

--- proconCsLib/Program.cs
+++ proconCsLib/Program.cs
@@ -10,6 +10,7 @@ using System.Text;
 using Problem = Tmp.Problem;
 using MyIO;
 using STL;
+using static Geometry.Geometry3D;
 
 namespace Tmp
 {
@@ -48,8 +49,8 @@ namespace Tmp
         RandomSFMT rand = Program.rand;
 
         // 使用する変数をここに書く
-        string S;
-        int a;
+//        string S;
+//        int a;
         /// <summary>
         /// 読み込み処理をここに書く
         /// </summary>
@@ -62,6 +63,7 @@ namespace Tmp
         /// </summary>
         void SolveOne()
         {
+            
         }
     }
 }
@@ -152,7 +154,7 @@ namespace MyIO
             else if (type == TypeCode.Decimal) return decimal.Parse(str);
             else if (type == TypeCode.Char) return str[0];
             else if (type == TypeCode.String) return str;
-            else if (type == Type.GetTypeCode(typeof(Point))) { int s, t; Read(out s, out t); return new Point(s, t); }
+            else if (type == Type.GetTypeCode(typeof(PointInt))) { int s, t; Read(out s, out t); return new PointInt(s, t); }
             else throw new Exception();
         }
         public T[] ReadMany<T>() { var type = Type.GetTypeCode(typeof(T)); return file.ReadLine().Split(sep, StringSplitOptions.RemoveEmptyEntries).Select(str => (T)Convert(str, type)).ToArray(); }
@@ -302,7 +304,7 @@ namespace MyIO
             for (int i = 0; i < n; ++i) a[i] = f();
             return a;
         }
-        
+
         public string[] next(int n) { return enumerate(n, next); }
         public double[] nextDouble(int n) { return enumerate(n, nextDouble); }
         public int[] nextInt(int n) { return enumerate(n, nextInt); }
@@ -482,10 +484,157 @@ namespace STL
     }
 }
 
+/// <summary>
+/// 幾何ライブラリ
+/// </summary>
+namespace Geometry
+{
+    #region 3次元幾何
+
 
     /// <summary>
+    /// 3次元幾何
     /// </summary>
+    static class Geometry3D
     {
+        public static double Eps = 1e-8;
+        public static double Inf = 1e12;
+        /// <summary>
+        /// 点/ベクトル(3次元)
+        /// </summary>
+        public struct Point3D
+        {
+            public double X;
+            public double Y;
+            public double Z;
+            public Point3D(double x, double y, double z) { X = x; Y = y; Z = z; }
+            public Point3D(double[] ls) { X = ls[0]; Y = ls[1]; Z = ls[2]; }
+
+            public static Point3D operator +(Point3D p) => new Point3D(p.X, p.Y, p.Z);
+            public static Point3D operator -(Point3D p) => new Point3D(-p.X, -p.Y, -p.Z);
+            public static Point3D operator /(Point3D p, double r) => new Point3D(p.X / r, p.Y / r, p.Z / r);
+            public static Point3D operator *(double r, Point3D p) => new Point3D(p.X * r, p.Y * r, p.Z * r);
+            public static Point3D operator *(Point3D p, double r) => new Point3D(p.X * r, p.Y * r, p.Z * r);
+            public static Point3D operator +(Point3D p, Point3D q) => new Point3D(p.X + q.X, p.Y + q.Y, p.Z + q.Z);
+            public static Point3D operator -(Point3D p, Point3D q) => new Point3D(p.X - q.X, p.Y - q.Y, p.Z - q.Z);
+        }
+
+        /// <summary>
+        /// 線分/直線(3次元)
+        /// </summary>
+         public struct Line3D
+        {
+            public Point3D a, b;
+            public Line3D(Point3D a, Point3D b) { this.a = a; this.b = b; }
+        }
+
+        /// <summary>
+        /// 内積
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double dot(Point3D a,Point3D b)
+        {
+            return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        }
+
+        /// <summary>
+        /// 外積
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Point3D cross(Point3D a, Point3D b)
+        {
+            return new Point3D(a.Y * b.Z - a.Z * b.Y, a.Z * b.X - a.X * b.Z, a.X * b.Y - a.Y * b.X);
+        }
+
+        public static double norm(Point3D a)
+        {
+            return dot(a, a);
+        }
+
+        public static double dist(Point3D a)
+        {
+            return Math.Sqrt(norm(a));
+        }
+
+        #region line
+        /// <summary>
+        /// 点が線分上にあるか
+        /// </summary>
+        /// <param name="l">線分</param>
+        /// <param name="p">点</param>
+        /// <returns></returns>
+        public static bool is_in_segment(Line3D l, Point3D p)
+        {
+            return Math.Abs(dist(l.a - p) + dist(l.b - p) - dist(l.a - l.b)) < Eps;
+        }
+
+        /// <summary>
+        /// 点の射影(点から直線に引いた垂線と線分の交点)
+        /// </summary>
+        /// <param name="l">直線</param>
+        /// <param name="p">点</param>
+        /// <returns></returns>
+        public static Point3D project_lp(Line3D l, Point3D p)
+        {
+            Point3D point = l.a, vec = l.b - l.a;
+            return point + dot(p - point, vec) / norm(vec) * vec;            
+        }
+
+        /// <summary>
+        /// 点と直線の距離
+        /// </summary>
+        /// <param name="l">直線</param>
+        /// <param name="p">点</param>
+        /// <returns></returns>
+        public static double distance_lp(Line3D l, Point3D p)
+        {
+            return dist(p - project_lp(l, p));
+        }
+
+        /// <summary>
+        /// 点と線分の距離
+        /// </summary>
+        /// <param name="l">線分</param>
+        /// <param name="p">点</param>
+        /// <returns></returns>
+        public static double distance_sp(Line3D l, Point3D p)
+        {
+            Point3D proj = project_lp(l, p);
+            if (is_in_segment(l, proj))
+            {
+                return dist(p - proj);
+            }
+            else
+            {
+                return Math.Min(dist(p - l.a), dist(p - l.b));
+            }
+        }
+
+        /// <summary>
+        /// 直線と直線の距離
+        /// </summary>
+        /// <param name="l"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static double distance_ll(Line3D l, Line3D m)
+        {
+            Point3D v = cross(l.b - l.a, m.b - m.a), p = m.a - l.a;
+            if (dist(v)<Eps)
+            {
+                return distance_lp(l, m.a);
+            }
+            else
+            {
+                return Math.Abs(dot(v, p)) / dist(v);
+            }
+        }
+        #endregion
+        
+        //TODO Plane関連以下の実装
     }
 
     #endregion
