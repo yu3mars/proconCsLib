435a0cb9da9394efffd3eb07464db606ed5ceef2
yu3mars <yu3mars@users.noreply.github.com>
2016-11-14 15:28:19 +0900

    add mod

--- proconCsLib/Program.cs
+++ proconCsLib/Program.cs
@@ -65,7 +65,11 @@ namespace Tmp
         /// </summary>
         void SolveOne()
         {
-
+            Mod a = new Mod(2);
+            Mod b = new Func.Mod(3);
+            Console.WriteLine((int)(a+b));
+            a += b;
+            Console.WriteLine((int)a);
         }
     }
 }
@@ -694,6 +698,178 @@ namespace Geometry
 
     #endregion
 }
+
+
+#region MyMath
+static partial class Func
+{
+    #region BasicMath
+    //TODO: 基本セットの整備(extgcdとか)
+
+    /// <summary>
+    /// 最大公約数
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    public static int GCD(int n, int m)
+    {
+        var a = Math.Abs(n);
+        var b = Math.Abs(m);
+        if (a < b) { var c = a; a = b; b = c; }
+        while (b > 0)
+        {
+            var c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+    /// <summary>
+    /// 使わないで
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    public static long GCD_oldver(long n, long m)
+    {
+        var a = Math.Abs(n);
+        var b = Math.Abs(m);
+        if (a < b) { var c = a; a = b; b = c; }
+        while (b > 0)
+        {
+            var c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+    /// <summary>
+    /// 最大公約数
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static long GCD(long a, long b)
+    {
+        var n = (ulong)Math.Abs(a); var m = (ulong)Math.Abs(b);
+        if (n == 0) return (long)m; if (m == 0) return (long)n;
+        int zm = 0, zn = 0;
+        while ((n & 1) == 0) { n >>= 1; zn++; }
+        while ((m & 1) == 0) { m >>= 1; zm++; }
+        while (m != n)
+        {
+            if (m > n) { m -= n; while ((m & 1) == 0) m >>= 1; }
+            else { n -= m; while ((n & 1) == 0) n >>= 1; }
+        }
+        return (long)n << Math.Min(zm, zn);
+    }
+    /// <summary>
+    /// 最大公約数
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static BigInteger GCD(BigInteger a, BigInteger b) { return BigInteger.GreatestCommonDivisor(a, b); } //comment out if AOJ
+
+    /// <summary>
+    /// 最小公倍数
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    public static long LCM(long n, long m) => Math.Abs((n / GCD(n, m)) * m);
+
+    /// <summary>
+    /// n以下の素数のリストを返す (get all primes less than or equal to n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static List<int> GetPrimes(int n)
+    {
+        if (n < 3) n = 3;
+        var m = (n - 1) >> 1;
+        var primes = new List<int>((int)(n / Math.Log(n)));
+        primes.Add(2);
+        var composites = new bool[m];
+        composites[0] = false;
+        for (var p = 0; p < m; p++)
+        {
+            if (!composites[p])
+            {
+                var pnum = 2 * p + 3;
+                primes.Add(pnum);
+                for (var k = 3 * p + 3; k < m; k += pnum) composites[k] = true;
+            }
+        }
+        return primes;
+    }
+
+    #endregion
+
+    #region Mod
+    const int mod = 1000000007;
+
+    public struct Mod
+    {
+        public long n;
+        public Mod(long m)
+        {
+            n = m;
+            if (n >= mod) n %= mod;
+            else if (n < 0) n = (n % mod + mod) % mod;
+        }
+        
+        public static explicit operator long(Mod m) { return m.n; }
+
+        //public static bool operator ==(Mod a, Mod b) { return a.n == b.n; }
+        //public static Mod operator +=(Mod a, Mod b) { a.n += b.n; if (a.n >= mod) a.n -= mod; return a; }
+        //public static Mod operator -=(Mod a, Mod b) { a.n -= b.n; if (a.n < 0) a.n += mod; return a; }
+        //public static Mod operator *=(Mod a, Mod b) { a.n = (int)(((long)a.n* b.n) % mod); return a; }
+        public static Mod operator +(Mod a, Mod b) { long tmp = a.n + b.n; if (tmp >= mod) tmp -= mod; return new Mod(tmp); }
+        public static Mod operator -(Mod a, Mod b) { long tmp = a.n - b.n; if (tmp < mod) tmp += mod; return new Mod(tmp); }
+        public static Mod operator *(Mod a, Mod b) { long tmp = ((a.n * b.n) % mod); return new Mod(tmp); }
+        public static Mod operator ^(Mod a, int n)
+        {
+            if (n == 0) return new Mod(1);
+            Mod res = (a * a) ^ (n / 2);
+            if (n % 2 == 1) res = res * a;
+            return res;
+        }
+        public static Mod operator /(Mod a, Mod b)
+        {
+            return a * new Mod(inv(b.n, mod));
+        }
+    }
+
+
+    static long inv(long a,long p)
+    {
+        return (a == 1 ? 1 : (1 - p * inv(p % a, a)) / a + p);
+    }
+
+    const int MAX_N = 1024000;
+    static bool modFact_Initialized = false;
+    static Mod[] fact = new Mod[MAX_N], factinv= new Mod[MAX_N];
+    static void init()
+    {
+        modFact_Initialized = true;
+        fact[0] = new Mod(1); factinv[0] = new Mod(1);
+        for (int i = 0; i < MAX_N; i++)
+        {
+            fact[i + 1] = fact[i] * new Mod(i + 1);
+            factinv[i + 1] = factinv[i] / new Mod(i + 1);
+        } 
+    }
+    static Mod comb(int a, int b)
+    {
+        if (modFact_Initialized == false) { init(); }
+        return fact[a] * factinv[b] * factinv[a - b];
+    }
+
+    #endregion
+}
+#endregion
 
 #region 未整理ライブラリ
 
@@ -2342,114 +2518,6 @@ class PointInt : Pair<int, int>
 }
 #endregion
 
-#region MyMath
-static partial class Func
-{
-    #region BasicMath
-    //TODO: 基本セットの整備(extgcdとか)
-
-    /// <summary>
-    /// 最大公約数
-    /// </summary>
-    /// <param name="n"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    public static int GCD(int n, int m)
-    {
-        var a = Math.Abs(n);
-        var b = Math.Abs(m);
-        if (a < b) { var c = a; a = b; b = c; }
-        while (b > 0)
-        {
-            var c = a % b;
-            a = b;
-            b = c;
-        }
-        return a;
-    }
-    /// <summary>
-    /// 使わないで
-    /// </summary>
-    /// <param name="n"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    public static long GCD_oldver(long n, long m)
-    {
-        var a = Math.Abs(n);
-        var b = Math.Abs(m);
-        if (a < b) { var c = a; a = b; b = c; }
-        while (b > 0)
-        {
-            var c = a % b;
-            a = b;
-            b = c;
-        }
-        return a;
-    }
-    /// <summary>
-    /// 最大公約数
-    /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static long GCD(long a, long b)
-    {
-        var n = (ulong)Math.Abs(a); var m = (ulong)Math.Abs(b);
-        if (n == 0) return (long)m; if (m == 0) return (long)n;
-        int zm = 0, zn = 0;
-        while ((n & 1) == 0) { n >>= 1; zn++; }
-        while ((m & 1) == 0) { m >>= 1; zm++; }
-        while (m != n)
-        {
-            if (m > n) { m -= n; while ((m & 1) == 0) m >>= 1; }
-            else { n -= m; while ((n & 1) == 0) n >>= 1; }
-        }
-        return (long)n << Math.Min(zm, zn);
-    }
-    /// <summary>
-    /// 最大公約数
-    /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static BigInteger GCD(BigInteger a, BigInteger b) { return BigInteger.GreatestCommonDivisor(a, b); } //comment out if AOJ
-
-    /// <summary>
-    /// 最小公倍数
-    /// </summary>
-    /// <param name="n"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    public static long LCM(long n, long m) => Math.Abs((n / GCD(n, m)) * m);
-
-    /// <summary>
-    /// n以下の素数のリストを返す (get all primes less than or equal to n)
-    /// </summary>
-    /// <param name="n"></param>
-    /// <returns></returns>
-    public static List<int> GetPrimes(int n)
-    {
-        if (n < 3) n = 3;
-        var m = (n - 1) >> 1;
-        var primes = new List<int>((int)(n / Math.Log(n)));
-        primes.Add(2);
-        var composites = new bool[m];
-        composites[0] = false;
-        for (var p = 0; p < m; p++)
-        {
-            if (!composites[p])
-            {
-                var pnum = 2 * p + 3;
-                primes.Add(pnum);
-                for (var k = 3 * p + 3; k < m; k += pnum) composites[k] = true;
-            }
-        }
-        return primes;
-    }
-
-    #endregion
-}
-#endregion
 
 #region Func
 
